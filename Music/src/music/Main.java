bcca94d631c4704204bc7f74dd8548705eee95a6
melllaem <laura.e.mella@student.jyu.fi>
2021-02-09 15:11:57 +0200

    scene ikkunoita.

---
+++ Music/src/music/Main.java
@@ -0,0 +1,35 @@
+package music;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * @author laura
+ * @version 1.2.2021
+ *
+ */
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MusicGUIView.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("music.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @param args ei käytössä
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ Music/src/music/MusicGUIController.java
@@ -0,0 +1,10 @@
+package music;
+
+/**
+ * @author laura
+ * @version 1.2.2021
+ *
+ */
+public class MusicGUIController {
+	//
+}
